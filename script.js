03b7c4c8adcd2d3069024762e9181b5e1f8b1447
Xeyyam <Xeyyam@Xeyyam>
2024-04-09 14:20:16 +0300

    all-completed

--- script.js
+++ script.js
@@ -25,7 +25,6 @@ btn.addEventListener("click", () => {
       chosenClass: "boxShadow",
       dragClass: "drag",
     });
-
     p.textContent = inp.value.trim();
     const imgElement = document.createElement("img");
     list.append(imgElement);
@@ -46,10 +45,56 @@ btn.addEventListener("click", () => {
     imgElement.addEventListener("click", () => {
       list.remove();
     });
+    inp.value = "";
   }
+
 });
+inp.addEventListener('keypress',(e)=>{
+  if(e.key === 'Enter'){
+    console.log(inp.value);
+  if (inp.value.trim() === "") {
+    alert("Write something");
+  } else {
+    listTask.style.display = "block";
+    const list = document.createElement("li");
+    const p = document.createElement("p");
+    
+    ol.append(list);
+    list.append(p);
+    const ulWrapper = document.getElementById("text-content");
+    new Sortable(ol, {
+      animation: 360,
+      chosenClass: "boxShadow",
+      dragClass: "drag",
+    });
+    p.textContent = inp.value.trim();
+    const imgElement = document.createElement("img");
+    list.append(imgElement);
+    list.style.display = "flex";
+    list.style.justifyContent = "space-between";
+    list.style.width = "250px";
+    imgElement.src = "/img/cancel.png";
+    imgElement.classList.add("imgelement");
+    tasks.push(p.textContent);
+    console.log(tasks);
+    imgElement.addEventListener("mouseover", (event) => {
+      event.target.src = "/img/cancelfull.png";
+    });
+
+    imgElement.addEventListener("mouseleave", (event) => {
+      event.target.src = "/img/cancel.png";
+    });
+    imgElement.addEventListener("click", () => {
+      list.remove();
+    });
+    inp.value = "";
+  }
+
+  }
+})
+
 inputImg.addEventListener("click", () => {
-  inp.value = " ";
+  inp.value = "";
 });
 inputImg.addEventListener("mouseover", (event) => {
   event.target.src = "/img/cancelfull.png";
